6140125eb57a765eaf9d0049f82cd4ddcde052b7
Josep Moltó <josepmolto@gmail.com>
2023-09-11 23:37:41 +0200

    Use decoder

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,25 @@
 ﻿using MQTTnet;
 using MQTTnet.Client;
+using Temperature.Receiver.Model;
 
 var mqttFactory = new MqttFactory();
-
+var decoder = new Decoder();
 
 using (var mqttClient = mqttFactory.CreateMqttClient())
 {
-    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("broker.hivemq.com").Build();
+    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
 
-    mqttClient.ApplicationMessageReceivedAsync += e =>
+    mqttClient.ApplicationMessageReceivedAsync += async e =>
     {
         Console.WriteLine("Received application message.");
-        var message = e.ApplicationMessage.Payload;
 
+        var message = e.ApplicationMessage.ConvertPayloadToString();
         Console.WriteLine(message);
 
-        return Task.CompletedTask;
+        var weatherInformation = await decoder.DecodeMessageAsync(e.ApplicationMessage.PayloadSegment);
+
+        Console.WriteLine($"Temperature: {weatherInformation.Temperature}");
+
     };
 
     await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
@@ -24,7 +28,7 @@ using (var mqttClient = mqttFactory.CreateMqttClient())
         .WithTopicFilter(
             f =>
             {
-                f.WithTopic("Ecowitt/2");
+                f.WithTopic("rtl_433/Ecowitt");
             })
         .Build();
 
