2c509dc16a4889b3cf307a8bae1fa869b73e95c3
Josep Moltó <josepmolto@gmail.com>
2023-09-19 20:26:18 +0200

    update docker compose

--- src/Program.cs
+++ src/Program.cs
@@ -2,40 +2,39 @@
 using MQTTnet.Client;
 using Temperature.Receiver.Model;
 
+
+
 var mqttFactory = new MqttFactory();
 var decoder = new Decoder();
 
-using (var mqttClient = mqttFactory.CreateMqttClient())
+using var mqttClient = mqttFactory.CreateMqttClient();
+var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("mosquitto").Build();
+
+mqttClient.ApplicationMessageReceivedAsync += async e =>
 {
-    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+    Console.WriteLine("Received application message.");
 
-    mqttClient.ApplicationMessageReceivedAsync += async e =>
-    {
-        Console.WriteLine("Received application message.");
+    var message = e.ApplicationMessage.ConvertPayloadToString();
+    Console.WriteLine(message);
 
-        var message = e.ApplicationMessage.ConvertPayloadToString();
-        Console.WriteLine(message);
+    var weatherInformation = await decoder.DecodeMessageAsync(e.ApplicationMessage.PayloadSegment);
 
-        var weatherInformation = await decoder.DecodeMessageAsync(e.ApplicationMessage.PayloadSegment);
+    Console.WriteLine($"Temperature: {weatherInformation.Temperature}");
 
-        Console.WriteLine($"Temperature: {weatherInformation.Temperature}");
+};
 
-    };
+await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
-    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+    .WithTopicFilter(
+        f =>
+        {
+            f.WithTopic("rtl_433/Ecowitt");
+        })
+    .Build();
 
-    var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
-        .WithTopicFilter(
-            f =>
-            {
-                f.WithTopic("rtl_433/Ecowitt");
-            })
-        .Build();
+await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
 
-    await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+Console.WriteLine("MQTT client subscribed to topic rtl_433/Ecowitt");
 
-    Console.WriteLine("MQTT client subscribed to topic.");
-
-    Console.WriteLine("Press enter to exit.");
-    Console.ReadLine();
-}
+await Task.Delay(-1).ConfigureAwait(false);
